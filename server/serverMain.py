4a1ffb07d3e9ff59752ea090abebe37eae74fa01
Sushant Bhattarai <swooshant@gmail.com>
2016-09-22 23:20:59 -0400

    combined server code, changed client decoding

--- server/serverMain.py
+++ server/serverMain.py
@@ -3,6 +3,7 @@ import pickle
 import hashlib
 import sys
 import wolfram
+import socket
 
 from cryptography.fernet import Fernet
 
@@ -18,8 +19,12 @@ while True:
 	print ("in server socket")
 	client, addr = s.accept() #Establish a connection with the client
 	print ('Got connection from',addr)
+	
 	data = client.recv(size)
+
+	print("pass1")
 	if data:
+		print("pass2")
 		server_payload = data
 		myData = pickle.loads(server_payload)
 		key, ciphertext, md5, servAddr = myData
@@ -31,17 +36,21 @@ while True:
 		question = f.decrypt(ciphertext)
 
 		app_id = "T72KQ9-LE373U33UW"
+
+		print(question)
+
 		result = wolfram.askWolfram(app_id, question)
+
+		print(result)
 
 		ciphertext = f.encrypt(str.encode(result))
 		checksum = hashlib.md5(ciphertext).hexdigest()
 
 		# build the payload tuple
-		payload = (cyphertext, checksum)
+		payload = (ciphertext, checksum)
 		# pickle up the payload
 		pickle_payload = pickle.dumps(payload)
 
 		client.send(pickle_payload)
 		client.close()
-
-
+		
