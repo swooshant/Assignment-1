d47fd33cd7d09056ed819476cc595d5affc37c45
ben lee <ben5995@vt.edu>
2016-09-22 18:45:10 -0700

    server socket

--- server/servSocket.py
+++ server/servSocket.py
@@ -2,19 +2,19 @@
 import socket #import the socket module
 
 def createServSocket():
+	print('in socket')
 	host = ""
 	port = 12397 # Reserve a port for your service
 	backlog = 5
 	size = 1024
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((host, port)) #Bind to the port
+	print('bind')
 	s.listen(backlog) #Wait for the client connection
 	while True:
 		print ("in server socket")
 		client, addr = s.accept() #Establish a connection with the client
 		print ('Got connection from',addr)
 		data = client.recv(size)
-		#extract the package in data
-		#close the socket
-
-		#client.close()
+		if data:
+			return data
